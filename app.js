6b2dd7ea2fb8c33d345f65ec711a23e1dda1388d
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2021-03-26 15:26:15 -0500

    this is my initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//     var currentScrollPos = window.pageYOffset;
+//     if (prevScrollpos > currentScrollPos) {
+//         document.getElementById("nav").style.top = "0";
+//         console.log('scrolling up')
+//     } else {
+//         document.getElementById("nav").style.top = "-100px";
+//         console.log('scrolling down')
+//     }
+//     prevScrollpos = currentScrollPos;
+    
+// }
+
+// responsive option 1 
+
+const toggle = document.querySelector('.toggle')
+const navigation = document.querySelector('.navigation')
+
+toggle.addEventListener('click', function() {
+    toggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+})
+
+// RESPONSIVE BTN
+// const menuBtn = document.getElementsByClassName('menu-btn')
+
+// const mobileMenu = document.getElementsByClassName('nav-menu')
+
+// const closeMenu = document.getElementsByClassName('close')
+
+
+
+// const clickedBtn = function() {
+//     mobileMenu[0].classList.toggle('active')
+// }
+
+// menuBtn[0].addEventListener('click', clickedBtn)
+
+// const clickedX = function() {
+//   mobileMenu[0].classList.remove('active')
+// }
+
+// closeMenu[0].addEventListener('click', clickedX)
+
+// Hero Slideshow
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
